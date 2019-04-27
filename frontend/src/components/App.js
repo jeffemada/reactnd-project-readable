e54ae69025ee1b78335b2aecfd6618d856afcf4c
Jefferson Madalena <jefferson.madalena@gmail.com>
2019-04-27 20:25:20 -0300

    Alterado componente principal da aplicação para uso do Redux.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
   render() {
-    return (
-      <div>     
-        Hello!
-      </div>
-    );
+    return <div>Hello!</div>;
   }
 }
 
-export default App;
+export default connect()(App);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
+import './index.css';
+import middleware from './middleware';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, middleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
