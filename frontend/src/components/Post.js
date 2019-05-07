94ff8fb71399071e96a62bea424d7875013c253d
Jefferson Madalena <jefferson.madalena@gmail.com>
2019-05-07 20:28:20 -0300

    Ajuste na exibição dos dados da postagem.

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import { FaArrowAltCircleDown, FaArrowAltCircleUp, FaCommentAlt } from 'react-icons/fa';
 import { connect } from 'react-redux';
+import TimeAgo from 'react-timeago';
 
 class Post extends Component {
   render() {
-    const { title, author, timestamp, voteScore, commentCount } = this.props.post;
+    const { title, author, timestamp, voteScore, commentCount, category } = this.props.post;
     return (
       <div className="row">
         <article className="col-md-12 post">
           <h3>{title}</h3>
           <p>
-            Posted by {author} {timestamp} ago
+            Posted by {author} <TimeAgo date={timestamp} />.
           </p>
           <p className="post-indicators">
             <FaArrowAltCircleDown />
-            <span>{voteScore}</span>            
+            <span>{voteScore}</span>
             <FaArrowAltCircleUp />
             <span className="separator" />
             <FaCommentAlt />
             <span>{commentCount} Comments</span>
+            <span className="separator" />
+            <span>{category}</span>
           </p>
         </article>
       </div>
