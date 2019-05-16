ab0ce776d81b8ea7409eeb6164ad231cedd2fce0
Jefferson Madalena <jefferson.madalena@gmail.com>
2019-05-15 22:47:56 -0300

    Mudança de layout menu.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,20 +8,13 @@ export default function Header() {
         <NavLink to="/" exact className="navbar-brand" alt="Go to home page" title="Home page">
           <img className="title-logo" src="../../images/logo.svg" alt="J Readable logo" />
         </NavLink>
-
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-          <span className="navbar-toggler-icon" />
-        </button>
-
-        <div className="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink to="/new" exact className="navbar-brand" alt="Go to create port page" title="Create post page">
-                Create post
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink to="/new" exact className="navbar-brand" alt="Go to create port page" title="Create post page">
+              Create post
+            </NavLink>
+          </li>
+        </ul>
       </nav>
     </header>
   );
